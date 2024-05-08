db25590dd6d9743c3ad61d52df78b3c628f0337f
Yelyzaveta-Korolekh <97096199+Yelyzaveta-Korolekh@users.noreply.github.com>
2024-05-08 17:44:09 +0300

    add player movement

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SnakeMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float _moveSpeed;
+    private Rigidbody2D _rb;
+    private Vector2 _moveDirection;
+    private SnakeInputSystem _inputActions;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _inputActions = new SnakeInputSystem();
+        _inputActions.Player.Movement.performed += HandleMovement;
+        _inputActions.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleMovement(InputAction.CallbackContext context)
     {
-        
+        _moveDirection = context.ReadValue<Vector2>();
     }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _rb.velocity = _moveDirection * _moveSpeed;
+    }    
 }
