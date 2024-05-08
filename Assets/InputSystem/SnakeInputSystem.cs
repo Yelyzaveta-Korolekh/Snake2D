843232be7d7752ae4c4030785ac373cdbd59bf74
Yelyzaveta-Korolekh <97096199+Yelyzaveta-Korolekh@users.noreply.github.com>
2024-05-08 18:41:48 +0300

    add simple rotate

--- Assets/InputSystem/SnakeInputSystem.cs
+++ Assets/InputSystem/SnakeInputSystem.cs
@@ -35,6 +35,15 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Rotation"",
+                    ""type"": ""Value"",
+                    ""id"": ""d71f1aad-0fe7-440e-bce6-f5c51a61923c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -92,6 +101,61 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
                     ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""f0658e8f-e9a7-4c71-ad5a-e88aac17ff1d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a04d6014-4f98-4c39-8028-7e09ebe6259d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""0e005ed6-5a40-48ce-bbaa-4ee8124f1447"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""a4ffba68-7e8a-4d8f-b904-7a9414486982"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""73b85302-7dd4-4ab7-82c2-98b53f6ddfe8"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
                 }
             ]
         }
@@ -101,6 +165,7 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Rotation = m_Player.FindAction("Rotation", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -163,11 +228,13 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Rotation;
     public struct PlayerActions
     {
         private @SnakeInputSystem m_Wrapper;
         public PlayerActions(@SnakeInputSystem wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Rotation => m_Wrapper.m_Player_Rotation;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -180,6 +247,9 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
             @Movement.started += instance.OnMovement;
             @Movement.performed += instance.OnMovement;
             @Movement.canceled += instance.OnMovement;
+            @Rotation.started += instance.OnRotation;
+            @Rotation.performed += instance.OnRotation;
+            @Rotation.canceled += instance.OnRotation;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -187,6 +257,9 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
             @Movement.started -= instance.OnMovement;
             @Movement.performed -= instance.OnMovement;
             @Movement.canceled -= instance.OnMovement;
+            @Rotation.started -= instance.OnRotation;
+            @Rotation.performed -= instance.OnRotation;
+            @Rotation.canceled -= instance.OnRotation;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -207,5 +280,6 @@ public partial class @SnakeInputSystem: IInputActionCollection2, IDisposable
     public interface IPlayerActions
     {
         void OnMovement(InputAction.CallbackContext context);
+        void OnRotation(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -9,13 +9,16 @@ public class SnakeMovement : MonoBehaviour
     [SerializeField] float _moveSpeed;
     private Rigidbody2D _rb;
     private Vector2 _moveDirection;
+    private Vector2 _rotation;
     private SnakeInputSystem _inputActions;
+    private const int _rotateDegree = 90;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _inputActions = new SnakeInputSystem();
         _inputActions.Player.Movement.performed += HandleMovement;
+        _inputActions.Player.Rotation.performed += RotateSnake;
         _inputActions.Enable();
     }
 
@@ -32,5 +35,23 @@ public class SnakeMovement : MonoBehaviour
     private void Move()
     {
         _rb.velocity = _moveDirection * _moveSpeed;
-    }    
+    }
+    
+    private void RotateSnake(InputAction.CallbackContext context)
+    {
+        _rotation = context.ReadValue<Vector2>();
+        ChooseRotateDirection(_rotation);
+    }
+
+    private void ChooseRotateDirection(Vector2 rotate)
+    {
+        if (rotate.x < 0)
+        {
+            transform.Rotate(0,0, _rotateDegree);
+        }
+        else if (rotate.x > 0) {
+            transform.Rotate(0, 0, -_rotateDegree);
+        }
+        
+    }
 }
